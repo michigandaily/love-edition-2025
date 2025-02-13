2b37b3bad10daed7cc55fb3a3679a522b75aeede
Alvin Jiang <alvinjg@umich.edu>
2025-02-12 23:54:23 -0500

    1. Created ArticleGallery component, added in functionality to click between different articles. 2. Need to implement responsiveness (only worked with px so far)

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,7 +1,31 @@
-import Article from './article';
+import ArticleGallery from './ArticleGallery';
+
+// testing purposes
+const articles = [
+  {
+    title: "John Cena",
+    author: "Super Idol",
+    date: "02/11/2025",
+  },
+  {
+    title: "LeGoat James",
+    author: "Lebron's Number One Fanboy",
+    date: "02/12/2025",
+  },
+  {
+    title: "Idk",
+    author: "Daily",
+    date: "02/13/2025",
+  },
+  {
+    title: "Article Name 3",
+    author: "Love Notes Article Author",
+    date: "02/14/2025",
+  }
+]
 
 const App = () => {
-	return <><Article title="Article Name" author="Love Notes Article Author" date="02/11/2025" photo="/path/to/photo.jpg" /></>;
+	return <><ArticleGallery articles={articles}/></>;
 };
 
 export default App;
