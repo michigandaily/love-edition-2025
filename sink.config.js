023e45611ff00f474bd5402c7dd9457ffecc51e5
Emily Chen <emychen@umich.edu>
2025-02-14 00:54:24 -0500

    set up sink

--- sink.config.js
+++ sink.config.js
@@ -10,9 +10,9 @@ export default defineConfig({
 		},
 		{
 			type: 'sheet',
-			id: '',
-			sheetId: '0',
-			output: '',
+			id: '11IcCL1XczOZ4WemixtNvGUEH9jClh_3qoEXy3b51ThM',
+			sheetId: '1241751428',
+			output: 'src/data/data.json',
 			auth: '~/.daily-google-services.json'
 		},
 		{
