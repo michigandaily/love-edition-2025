51036d0b0ec5362bf2421354fb6601ae21196198
Emily Chen <emychen@umich.edu>
2025-02-16 20:43:20 -0500

    fix mobile design

--- src/js/components/LandingPage.tsx
+++ src/js/components/LandingPage.tsx
@@ -1,34 +1,3 @@
-// import React from "react";
-// import backgroundImage from '../assets/background.png';
-
-// const LandingPage = () => {
-//   return (
-// 		<div className="relative min-h-screen flex items-center justify-center bg-brown-100">
-// 			{/* Background Image */}
-// 			<div
-// 				className="absolute inset-0 bg-cover bg-center"
-// 				style={{
-// 					backgroundImage: `url(${backgroundImage})`
-// 				}}
-// 			></div>
-
-// 			{/* Overlay to enhance text visibility */}
-// 			<div className="absolute inset-0 bg-brown-900 bg-opacity-30"></div>
-
-// 			{/* Content */}
-// 			<div className="relative z-10 text-center p-6">
-// 				<div className="bg-white px-6 py-4 shadow-lg rounded-lg">
-// 					<h1 className="text-xl font-semibold text-brown-800">Love Notes</h1>
-// 				</div>
-// 			</div>
-
-// 			<img src={backgroundImage} alt="Alternative text" />
-// 		</div>
-// 	);
-// };
-
-// export default LandingPage;
-
 import '../../css/main.scss'; // Import the external CSS file
 
 const LandingPage = () => {
